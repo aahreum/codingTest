bd78164706554c152b973d8dd41928481e6cc6e0
이아름 <117738875+aahreum@users.noreply.github.com>
2024-03-19 22:30:26 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,25 +1,9 @@
-const sort = (array) => {
-    return array.sort((a, b) => a - b);
-}
-
 const solution = (n) => {
     let answer = [];
-    
-    if (n % 2 === 0) {
-        n--;
-        answer.push(n)
-        while (n > 1) {
-            n -= 2;
-            answer.push(n)
-        }
-        return sort(answer);
-        
-    } else if (n % 2 === 1) {
-        answer.push(n)
-           while (n > 1) {
-            n -= 2;
-            answer.push(n)
-        }
-        return sort(answer);
+    for (let i = 1; i <= n; i += 2) {
+        answer.push(i)
     }
+    
+    return answer;
 }
+
