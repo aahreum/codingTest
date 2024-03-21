977eb6e513b26ea88f4db70e4ce6a71ba70e5c53
이아름 <117738875+aahreum@users.noreply.github.com>
2024-03-21 23:39:25 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,6 +1,5 @@
 const solution = (my_string) => {
-    let array = my_string.split('');
-    let answer = array.reverse().join('')
+    let answer = [...my_string].reverse().join('')
        
     return answer;
 }
