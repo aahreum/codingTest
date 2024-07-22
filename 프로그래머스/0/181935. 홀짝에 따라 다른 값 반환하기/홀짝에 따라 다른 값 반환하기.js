658cf9cd365381a976a86e8e6bd88c15033ec2ae
이아름 <117738875+aahreum@users.noreply.github.com>
2024-07-22 11:59:29 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.14 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,13 @@
+const solution = (n) => {
+    let answer = 0;
+    if (n % 2 === 1) {
+        for (let i = n; i > 0; i -= 2) {
+            answer += i;
+        }
+    } else {
+        for (let i = 2; i <= n; i += 2) {
+            answer += i ** 2;
+        }
+    }
+      return answer;
+}
