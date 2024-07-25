190f3a6c3ce2c8a0aba62d01a212fe5f79b8da17
이아름 <117738875+aahreum@users.noreply.github.com>
2024-07-25 09:24:18 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,10 +1,9 @@
 const solution = (age) => {
-    const abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-    const array = String(age).split('').map(Number)
+    const abc = 'abcdefghij';
     let answer = ''
     
-    for (let i = 0; i < array.length; i++) {
-        answer += abc[array[i]]
+    for (let i = 0; i < String(age).length; i++) {
+        answer += abc[String(age)[i]]
     }
     
     return answer;
