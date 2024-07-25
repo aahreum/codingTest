5a52783b056e1bb7d70c86cacd5dd68a4949ef60
이아름 <117738875+aahreum@users.noreply.github.com>
2024-07-25 10:26:05 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -5,3 +5,10 @@ const solution = (dot) => {
     
     return answer;
 }
+
+// 참고
+// const solution = (dot) => {
+//     const [num,num2] = dot;
+//     const check = num * num2 > 0;
+//     return num > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
+// }
