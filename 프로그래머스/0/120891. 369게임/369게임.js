f7fb47708f187b47ca9f91b816afe5761990bb57
이아름 <117738875+aahreum@users.noreply.github.com>
2024-07-26 10:28:14 +0900

    [level 0] Title: 369게임, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,5 +1,3 @@
 const solution = (order) => {
-    let answer = 0;
-    String(order).split('').filter((w) => w === '3' || w === '6' || w === '9' ? answer++ : 0 )
-    return answer;
+    return String(order).split('').filter((w) => w === '3' || w === '6' || w === '9').length
 }
