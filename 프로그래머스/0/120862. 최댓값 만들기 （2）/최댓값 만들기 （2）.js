27563d77df77495570cf90435e9a724b610c04ba
이아름 <117738875+aahreum@users.noreply.github.com>
2024-07-26 10:39:13 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -2,6 +2,5 @@ const solution = (numbers) => {
     numbers.sort((a, b) => (b - a))
     let a = numbers[0] * numbers[1]
     let b = numbers[numbers.length - 1] * numbers[numbers.length - 2]
-    let answer = a > b ? a : b
-    return answer;
+    return Math.max(a, b);
 }
