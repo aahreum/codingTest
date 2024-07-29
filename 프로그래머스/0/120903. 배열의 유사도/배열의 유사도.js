73ff806a35dc87fa99f04720060dcb62e58ce669
이아름 <117738875+aahreum@users.noreply.github.com>
2024-07-29 10:30:53 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,7 +1,3 @@
 const solution = (s1, s2) => {
-    let map = {};
-    let arr = s1.concat(s2);
-    arr.forEach((el) => map[el] = (map[el] || 0) + 1);
-    
-    return [...new Set(arr.filter((el) => map[el] !== 1))].length;
+    return s1.filter((x) => s2.includes(x)).length;
 }
