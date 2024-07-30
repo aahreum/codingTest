3299ecd7b2231d64209371522f1d99e56f6401bc
이아름 <117738875+aahreum@users.noreply.github.com>
2024-07-30 10:49:46 +0900

    [level 0] Title: 팩토리얼, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,14 @@
+const solution = (n) => {
+    let answer = 1;
+    
+    for (let i = 1; i <= n; i++) {
+        answer *= i       
+        if (answer === n) {
+            return i
+        } else if (answer > n) {
+            return i - 1
+        }    
+    }
+    
+    return answer;
+}
