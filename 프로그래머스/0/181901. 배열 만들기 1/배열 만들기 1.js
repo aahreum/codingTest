48d428d1d6ca77de4e0fa085e63ab2a503058b9e
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-05 10:41:34 +0900

    [level 0] Title: 배열 만들기 1, Time: 37.73 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -0,0 +1,9 @@
+const solution = (n, k) => {
+    let answer = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % k === 0) {
+            answer.push(i)
+        }
+    } 
+    return answer;
+}
