53383dec1cc69e62a82253abcfd145f1e6537b69
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-11 09:48:42 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,13 @@
+const solution = (n) => {
+    let answer = [];
+    while (n > 1) {
+        answer.push(n)
+        if (n % 2 === 0) {
+            n = n / 2
+        } else {
+            n = 3 * n + 1
+        } 
+    }
+    answer.push(1)
+    return answer;
+}
