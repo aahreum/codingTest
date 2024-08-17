0df6283bfa0ac060a234a52e356a2d793ef2125f
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-17 15:55:05 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -4,3 +4,5 @@ const solution = (date1, date2) => {
     
     return y1 < y2 || (y1 <= y2 && m1 < m2) || (y1 <= y2 && m1 <= m2 && d1 < d2) ? 1 : 0
 }
+
+// const solution = (date1, date2) => new Date(date1) < new Date(date2) ? 1 : 0
