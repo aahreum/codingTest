2f93f490156cc560b016697ac5241764c235b393
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-18 11:27:53 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 22.65 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,9 @@
+const solution = (a, b) => {
+    let answer = 0;
+    let max = Math.max(a, b)
+    let min = Math.min(a, b)
+    for (let i = min; i <= max; i++) {
+        answer += i;
+    }
+    return answer;
+}
