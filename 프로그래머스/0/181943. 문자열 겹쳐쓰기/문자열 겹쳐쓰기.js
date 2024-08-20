ed07311084b964e5160b6cffe1112e86935136a3
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-20 10:00:02 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,4 @@
+const solution = (my_string, overwrite_string, s) => {
+    let answer = my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length);    
+    return answer;
+}
