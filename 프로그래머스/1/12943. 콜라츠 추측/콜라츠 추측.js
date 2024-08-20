4eb9876b09e0b342a6b2e177cc8cdab54a3c7002
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-20 10:31:07 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,19 @@
+let solution = (num) => {
+    let count = 0;
+    if (num === 1) {
+        return 0
+    }
+    while (num > 1) {
+        if (count === 500) {
+            return -1
+        }
+        if (num % 2 === 0) {
+            num = num / 2
+            count++
+        } else {
+            num = num * 3 + 1
+            count++
+        }
+    }
+    return count;
+}
