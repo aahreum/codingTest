37cbd8896fa8da903252f2a6b7819f84f2f39669
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-22 07:59:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.54 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,11 @@
+const solution = (n) => {
+    let answer = '';
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 !== 0) {
+            answer += '수'
+        } else {
+            answer += '박'
+        }
+    }
+    return answer;
+}
