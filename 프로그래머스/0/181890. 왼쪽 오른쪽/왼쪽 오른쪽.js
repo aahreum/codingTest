4d2d98e19c90304f7a8deb17345ae7583689aa58
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-23 12:58:20 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,17 +1,7 @@
 const solution = (str_list) => {
-    if (!str_list.includes('l') && !str_list.includes('r')) {
-        return [];
+    for (let i = 0; i < str_list.length; i++) {
+        if (str_list[i] === 'l') return str_list.slice(0, i);
+        if (str_list[i] === 'r') return str_list.slice(i + 1);
     }
-    
-    let lIndex = str_list.indexOf('l');
-    let rIndex = str_list.indexOf('r');
-    
-    if (str_list.includes('l') && !str_list.includes('r')) {
-        return str_list.slice(0, lIndex);
-    }
-    if (!str_list.includes('l') && str_list.includes('r')) {
-        return str_list.slice(rIndex + 1);
-    }
-    
-    return lIndex < rIndex ? str_list.slice(0, lIndex) : str_list.slice(rIndex + 1);
+    return [];
 }
