376055aed4a517e1e8e3a0c5d2a3c4fcb45c3048
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-25 09:24:19 +0900

    [level 0] Title: 코드 처리하기, Time: 8.12 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -2,18 +2,10 @@ const solution = (code) => {
     let ret = '';
     let mode = 0;
     for (let i = 0; i < code.length; i++) {
-        if (mode === 0) {
-            if (i % 2 === 0 && code[i] !== '1') {
-                ret += code[i]
-            } else if (code[i] === '1') {
-                mode = 1;
-            }
-        } else {
-            if (i % 2 !== 0 && code[i] !== '1') {
-                ret += code[i]
-            } else if (code[i] === '1') {
-                mode = 0;
-            }
+        if (code[i] === '1') {
+            mode = 1 - mode;
+        } else if ((mode === 0 && i % 2 === 0) || (mode === 1 && i % 2 !== 0)) {
+            ret += code[i];
         }
     }
     
