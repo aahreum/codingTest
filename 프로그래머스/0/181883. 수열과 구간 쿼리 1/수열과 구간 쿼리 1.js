2ffb34300b286aa275e9177cfbb0a171779a5827
이아름 <117738875+aahreum@users.noreply.github.com>
2024-08-26 12:44:05 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 4.40 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,6 +1,6 @@
 const solution = (arr, queries) => {
     queries.forEach(([s, e]) => {
-        for (let i = 0; i <= arr.length; i++) {
+        for (let i = 0; i < arr.length; i++) {
             if (s <= i && i <= e) {
                 arr[i] += 1;
             }
