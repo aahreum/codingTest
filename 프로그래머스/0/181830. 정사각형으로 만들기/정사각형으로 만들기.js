77b50a1d4a3180d4bb0fb3024d7f253b6b46cf6e
이아름 <117738875+aahreum@users.noreply.github.com>
2024-09-01 10:27:41 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.30 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,11 +1,10 @@
 const solution = (arr) => {
+    let diff = Math.abs(arr.length - arr[0].length);
     if (arr.length > arr[0].length) {
-        let diff = arr.length - arr[0].length;
         for (let i = 0; i < arr.length; i++) {
             arr[i].push(...Array(diff).fill(0));
         }
     } else if (arr.length < arr[0].length) {
-        let diff = arr[0].length - arr.length;
         let array = new Array(arr[0].length).fill(0);
         arr.push(...Array(diff).fill(array));
     }
