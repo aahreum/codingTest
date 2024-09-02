a6d38774a59fbfc2f51730f82c2495141b607fd8
이아름 <117738875+aahreum@users.noreply.github.com>
2024-09-02 10:02:05 +0900

    [level 2] Title: 올바른 괄호, Time: 27.65 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,16 @@
+const solution = (s) =>{
+    let answer = true;
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(s[i])
+        } else if (s[i] === ')') {
+            if (stack.length === 0) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+
+    return stack.length === 0;
+}
