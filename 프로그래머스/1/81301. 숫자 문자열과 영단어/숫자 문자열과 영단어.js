55060a90bbdbc5b0604f611a9827d2a864927fd2
이아름 <117738875+aahreum@users.noreply.github.com>
2024-09-03 10:09:25 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,21 @@
+const solution = (s) => {
+    const numWords = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    };
+    
+    for (let word in numWords) {
+        let regex = new RegExp(word, 'g');
+        s = s.replace(regex, numWords[word])
+    }
+    
+    return Number(s);
+}
