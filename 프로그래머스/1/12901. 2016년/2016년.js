5204329e11df719afb3980491c3ac41b375c1213
이아름 <117738875+aahreum@users.noreply.github.com>
2024-09-12 12:14:09 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,7 +1,5 @@
 const solution = (a, b) => {
     let day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    a = String(a).length === 1 ? String(a).padStart(2, '0') : String(a);
-    b = String(b).length === 1 ? String(b).padStart(2, '0') : String(b);
     let index = new Date(`2016-${a}-${b}`).getDay();
     return day[index];
 }
