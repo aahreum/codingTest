be13186a1ae07e91b49d8b5efffb019546490db9
이아름 <117738875+aahreum@users.noreply.github.com>
2024-09-27 14:01:01 +0900

    [level 2] Title: 의상, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,15 @@
+const solution = (clothes) => {
+    let answer = 0;
+    let obj = clothes.reduce((acc, [value, key]) => {
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(value);
+        return acc;
+    }, {})
+    let set = Object.values(obj).reduce((acc, arr) => {
+        return acc * (arr.length + 1);
+    }, 1)
+    
+    return set - 1;
+}
